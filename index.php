9a769d2983f6628682e7ba9e3c4171022bce5598
Rait Rips <rait.rips@tptlive.ee>
2025-09-02 11:31:48 +0300

    Add array examples

--- index.php
+++ index.php
@@ -1,29 +1,36 @@
 <?php
-
-echo "Hello World!\n";
-echo("Hello World!\n");
-
-$var = 1;
-echo $var;
-echo 'var is $var';
-echo "\nvar is $var\n";
-echo $var . "\n";
-$num = 1;
-$num = $num + 1;
-$num++;
-$num--;
-$num += 1;
-$num -= 1;
-$num *= 2;
-$num /= 3;
-$num = 13;
-$num %= 5;
-$num = 1.3;
-$num = 3;
-$num **= 2;
-$num = true;
-$num = false;
-$bar;
-$bar = NULL;
-
-var_dump($bar);
+$array = array(1, 2, 3, 4);
+$array = [1, 2, 3, 4];
+$array = [
+    1, 
+    'hello', 
+    true, 
+    NULL, 
+    [1, 2, 3]
+];
+$array = [
+    'name' => 'Kaspar',
+    'age' => 32
+];
+$array = [
+    6 => 'lol',
+    1,
+    2,
+    3,
+    'name' => 'Kaspar',
+    'age' => 32,
+    4,
+    5
+];
+var_dump($array['name']);
+$array = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+];
+var_dump($array[2][1]);
+$array = [1, 2, 3];
+array_push($array, 4);
+array_push($array, 5, 6, 7);
+$array[] = 8;
+var_dump($array);
