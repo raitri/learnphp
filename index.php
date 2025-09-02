194f0959beaa9658ada992175e528013df2ba9e0
Rait Rips <rait.rips@tptlive.ee>
2025-09-02 11:13:43 +0300

    Add types examples

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+echo "Hello World!\n";
+echo("Hello World!\n");
+
+$var = 1;
+echo $var;
+echo 'var is $var';
+echo "\nvar is $var\n";
+echo $var . "\n";
+$num = 1;
+$num = $num + 1;
+$num++;
+$num--;
+$num += 1;
+$num -= 1;
+$num *= 2;
+$num /= 3;
+$num = 13;
+$num %= 5;
+$num = 1.3;
+$num = 3;
+$num **= 2;
+$num = true;
+$num = false;
+$bar;
+$bar = NULL;
+
+var_dump($bar);
