fc7fb5a58e39ebf6b69d02077817187ee48f7be4
Rait Rips <rait.rips@tptlive.ee>
2025-09-09 10:19:46 +0300

    Add boolean math example

--- index.php
+++ index.php
@@ -1,36 +1,18 @@
 <?php
-$array = array(1, 2, 3, 4);
-$array = [1, 2, 3, 4];
-$array = [
-    1, 
-    'hello', 
-    true, 
-    NULL, 
-    [1, 2, 3]
-];
-$array = [
-    'name' => 'Kaspar',
-    'age' => 32
-];
-$array = [
-    6 => 'lol',
-    1,
-    2,
-    3,
-    'name' => 'Kaspar',
-    'age' => 32,
-    4,
-    5
-];
-var_dump($array['name']);
-$array = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-];
-var_dump($array[2][1]);
-$array = [1, 2, 3];
-array_push($array, 4);
-array_push($array, 5, 6, 7);
-$array[] = 8;
-var_dump($array);
+
+$bool = true && false;
+$bool = true || false;
+$bool = !false;
+$bool = (true && false || false) && !(true || false && true);
+$bool = 10 > 5;
+$bool = 10 < 5;
+$bool = 10 > 10;
+$bool = 10 < 10;
+$bool = 10 == 10;
+$bool = 10 >= 10;
+$bool = 10 <= 10;
+$bool = 10 == '10';
+$bool = 10 === '10';
+$bool = 10 !== '10';
+$bool = 10 != '10';
+var_dump($bool);
