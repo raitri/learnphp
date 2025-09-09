fcf721cb99819bf0141e0113795129877e5920a7
Rait Rips <rait.rips@tptlive.ee>
2025-09-09 10:30:15 +0300

    Add if and switch examples

--- index.php
+++ index.php
@@ -1,18 +1,36 @@
 <?php
+$num = 15;
+if($num > 10) {
+    var_dump('Suurem');
+} elseif ($num < 10) {
 
-$bool = true && false;
-$bool = true || false;
-$bool = !false;
-$bool = (true && false || false) && !(true || false && true);
-$bool = 10 > 5;
-$bool = 10 < 5;
-$bool = 10 > 10;
-$bool = 10 < 10;
-$bool = 10 == 10;
-$bool = 10 >= 10;
-$bool = 10 <= 10;
-$bool = 10 == '10';
-$bool = 10 === '10';
-$bool = 10 !== '10';
-$bool = 10 != '10';
-var_dump($bool);
+} else {
+
+}
+
+$day = date('w', strtotime('09.09.2025'));
+var_dump($day);
+
+switch($day) {
+    case 1:
+        var_dump('esmaspäev');
+        break;
+    case 2:
+        var_dump('teisipäev');
+        break;
+    case 3:
+        var_dump('kolmapäev');
+        break;
+    case 4:
+        var_dump('neljapäev');
+        break;
+    case 5:
+        var_dump('reede');
+        break;
+    case 6:
+    case 7:
+        var_dump('pidupäev');
+        break;
+    default:
+        var_dump('imelik päev');
+}
