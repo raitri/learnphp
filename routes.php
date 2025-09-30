27533efb0e8f2880163fbad12c785d128bf207cf
Rait Rips <rait.rips@tptlive.ee>
2025-09-30 11:45:29 +0300

    Add db

--- routes.php
+++ routes.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Controllers\PostsController;
 use App\Controllers\PublicController;
 use App\Router;
 
@@ -10,3 +11,5 @@ Router::get('/us', [PublicController::class, 'us']);
 Router::get('/form', [PublicController::class, 'form']);
 
 Router::post('/answer', [PublicController::class, 'answer']);
+
+Router::get('/posts', [PostsController::class, 'index']);
